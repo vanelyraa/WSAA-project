e413eca8b21d4201e2ba9e649a06b7f5d91485f8
vanelyra <vanelyra@hotmail.com>
2026-04-19 11:12:43 +0100

    Adding functions to DAO

--- trackerDAO.py
+++ trackerDAO.py
@@ -22,7 +22,7 @@ class TrackerDAO:
             password=self.password,
             database=self.database,
         )
-        self.cursor = self.connection.cursor(dictionary=True)
+        self.cursor = self.connection.cursor()
         return self.cursor
 
     def closeAll(self):
@@ -31,16 +31,84 @@ class TrackerDAO:
 
     def getAll(self):
         cursor = self.getcursor()
-        sql = "SELECT * FROM shipments"
+        sql = "select * from shipments"
         cursor.execute(sql)
         results = cursor.fetchall()
-        #returnArray = []
+        returnArray = []
         #print(results)
-        #for result in results:
+        for result in results:
             #print(result)
-            #returnArray.append(self.convertToDictionary(result))
+            returnArray.append(self.convertToDictionary(result))
+        
         self.closeAll()
-        return results
+        return returnArray
     
+    def findByID(self, id):
+        cursor = self.getcursor()
+        sql = "select * from shipments where id = %s"
+        values = (id,)
+
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        returnvalue = self.convertToDictionary(result)
+        self.closeAll()
+        return returnvalue
+
+    def create(self, shipment):
+        cursor = self.getcursor()
+        sql = "insert into shipments (status, planned_eta, supplier_code, supplier_name, actual_eta, item_code)"
+        values = (
+            shipment.get("status"),
+            shipment.get("planned_eta"),
+            shipment.get("supplier_code"),
+            shipment.get("supplier_name"),
+            shipment.get("actual_eta"),
+            shipment.get("item_code")
+        )
+        cursor.execute(sql, values)
+
+        self.connection.commit()
+        newid = cursor.lastrowid
+        shipment["id"] = newid
+
+        self.closeAll()
+        return shipment
+
+def update(self, id, shipment):
+    cursor = self.getcursor()
+    sql = "update shipments set status=%s, planned_eta=%s, supplier_code=%s, supplier_name=%s, actual_eta=%s, item_code=%s where id =%s"
+    print(f"update shipment {shipment}")
+    values = (
+        shipment.get("status"),
+        shipment.get("planned_eta"),
+        shipment.get("supplier_code"),
+        shipment.get("supplier_name"),
+        shipment.get("actual_eta"),
+        shipment.get("item_code"),
+        id
+    )
+    cursor.execute(sql, values)
+    self.connection.commit()
+    self.closeAll()
+        
+def delete(self, id):
+    cursor = self.getcursor()
+    sql="delete from shipments where id = %s"
+    values = (id,)
+    cursor.execute(sql, values)
+    self.connection.commit()
+    self.closeAll()
+
+def convertToDictionary(self, resultLine):
+    attkeys=['id','status','planned_eta','supplier_code','supplier_name','actual_eta','item_code']
+    shipment = {}
+    currentkey = 0
+    for attrib in resultLine:
+        shipment[attkeys[currentkey]] = attrib
+        currentkey = currentkey + 1 
+    return shipment
 
 trackerDAO = TrackerDAO()
+
+#Reference:
+#Python anywhere lecture
