6a3b93bffa0ee28a60a3aa2bee698500320b24c4
vanelyra <vanelyra@hotmail.com>
2026-05-04 13:31:59 +0100

    createsquema.py file update

--- createsquema.py
+++ createsquema.py
@@ -1,11 +1,20 @@
 import sqlite3
 import dbconfig as cfg
+from os import path
+
 database = cfg.sqlite["database"]
 
-con = sqlite3.connect(database)
+ROOT = path.dirname(path.realpath(__file__))
+db_path = path.join(ROOT, database)
+schema_path = path.join(ROOT, "schema.sql")
+
+con = sqlite3.connect(db_path)
 cur = con.cursor()
-with open("schema.sql", "r") as fp:
+with open(schema_path, "r") as fp:
     sql = fp.read()
 
 cur.executescript(sql)
 con.close()
+
+
+# PythonAnywhere FileNotFoundError:'schema.sql':https://stackoverflow.com/questions/34510196/python-flask-how-to-access-sqlite-database-db-on-pythonanywhere-com-without-sp
